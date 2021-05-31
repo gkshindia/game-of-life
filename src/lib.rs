a79eaa88d60b5df771b428444b778e25f4edb069
Gourav Kumar Singh <gksingh@14weeks.in>
2021-05-31 10:01:15 +0530

    Live neighbour count

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,38 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Bonjour {name}!", name=name));
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        return (row * self.width + column) as usize;
+    }
+
+    fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+                let neighbour_row = (row + delta_row) % self.height;
+                let neighbour_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbour_row, neighbour_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        return count;
+    }
 }
